54c44f2bcb845f23e1dd4d0ee6c0761726813a14
rPbl3yH <tnv2205@gmail.com>
2022-04-16 16:47:45 +0500

    feat: add 2 points: Science, Physical

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Building : PlacebleObject
+public abstract class Building : PlacebleObject
 {
+    
     public override void OnPointerClick(PointerEventData eventData)
     {
         BuildSystem.InitializePlacements(transform.position,  gameObject.GetComponent<Building>());
--- Assets/Scripts/SchoolBuilding.cs
+++ Assets/Scripts/SchoolBuilding.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class SchoolBuilding : Building
 {
+    public void Initialize(byte sciencePoint, byte physicalPoint)
+    {
+        _sciencePoint = sciencePoint;
+        _physicalPoint = physicalPoint;
+    }
+    
+    private byte _sciencePoint;
+    private byte _physicalPoint;
+    
     
 }
