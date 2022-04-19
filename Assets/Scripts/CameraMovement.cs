8f7db20e569f52f07741a8080d4ae853f3150a52
rPbl3yH <tnv2205@gmail.com>
2022-04-19 13:35:40 +0500

    feat: delete CameraZoomnig, add Zoom for camera in CameraMovement fix: camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,27 +5,23 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Camera _cameraMain;
-
     [Header("Target GameObject")]
     [SerializeField] private Transform _target;
     [SerializeField] private Vector3 _offset;
     
     [SerializeField] private float _sensitive = 3;
     [SerializeField] private float _limitY = 60;
-    
-    [Header("Zoom setting")]
-    [SerializeField] private float _zoom = 0.25f;
-    [SerializeField] private float _zoomMax = 10;
-    [SerializeField] private float _zoomMin = 3;
+
+    [SerializeField] private float _zoomMax;
+    [SerializeField] private float _zoomMin;
+    [SerializeField] private float _zoom;
     
     private float _x, _y;
+    
     private Touch _touch;
     
     void Awake()
     {
-        _cameraMain = GetComponent<Camera>();
-        
         Initialize();
     }
     
@@ -34,44 +30,31 @@ public class CameraMovement : MonoBehaviour
         if (Input.touchCount == 1)
         {
             _touch = Input.GetTouch(0);
-
             if (_touch.phase == TouchPhase.Moved)
             {
-                Vector3 movePos = new Vector3(
-                    transform.position.x + _touch.deltaPosition.x * -_sensitive * Time.deltaTime,
-                    transform.position.y,
-                    transform.position.z + _touch.deltaPosition.y * -_sensitive * Time.deltaTime);
-
-                Vector3 distance = movePos - _target.position;
-
-                if (distance.magnitude < _zoomMax)
-                {
-                    transform.position = movePos;
-                }
+                float deltaY = _touch.deltaPosition.y;
+                if(deltaY > 0) _offset.z += _zoom;
+                else if(deltaY < 0) _offset.z -= _zoom;
+                
+                _offset.z = Mathf.Clamp(_offset.z, -Mathf.Abs(_zoomMax), -Mathf.Abs(_zoomMin));
+                
+                _x = transform.localEulerAngles.y - _touch.deltaPosition.x / 100 * _sensitive;
+                
+                transform.localEulerAngles = new Vector3(_y, _x, 0);
+                transform.position = transform.localRotation * _offset + _target.position;
             }
         }
         
-        
-        
-        // if(Input.GetAxis("Mouse ScrollWheel") > 0) _offset.z += _zoom;
-        // else if(Input.GetAxis("Mouse ScrollWheel") < 0) _offset.z -= _zoom;
-        // _offset.z = Mathf.Clamp(_offset.z, -Mathf.Abs(_zoomMax), -Mathf.Abs(_zoomMin));
-        //
-        // _x = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * _sensitive;
-        // _y += Input.GetAxis("Mouse Y") * _sensitive;
-        // _y = Mathf.Clamp (_y, -_limitY, _limitY);
-        // transform.localEulerAngles = new Vector3(-_y, _x, 0);
-        // Vector3 pos = transform.localRotation * _offset + _target.position;
-        // transform.position = Vector3.Lerp(transform.position, pos, 5 /_sensitive * Time.deltaTime);
     }
+    
+    
 
     private void Initialize()
     {
-        
         _limitY = Math.Abs(_limitY);
         if (_limitY > 90) _limitY = 90;
-
-        //_offset = new Vector3(_offset.x, _offset.y, -Mathf.Abs(_zoomMax)/ 2);
-        //transform.position = _target.position + _offset;
+        _y = transform.localEulerAngles.x;
+        
+        transform.position = _target.position + _offset;
     }
 }
